09f233b2648dcab88e7bf6e200fe88a33ca898f4
Sean Winston <seanwinston@gmail.com>
2024-11-17 18:15:41 +0000

    feat: create new UI class and add setupPlateau method

--- src/main/java/org/example/UI.java
+++ src/main/java/org/example/UI.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class UI {
+    private State state;
+    private MissionControl missionControl;
+    private Scanner scanner;
+
+    public UI() {
+        state = State.SETUP_PLATEAU;
+        scanner = new Scanner(System.in);
+    }
+
+    public State setupPlateau(){
+        while(true){
+            try{
+                System.out.println("Enter plateau size");
+                String input = scanner.nextLine();
+                PlateauSize plateauSize = InputParser.parsePlateau(input);
+                missionControl = new MissionControl(new Plateau(plateauSize));
+                State nextState = State.DEPLOY_ROVER;
+                return nextState;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid size, please reenter");
+            }
+        }
+    }
+
 }
