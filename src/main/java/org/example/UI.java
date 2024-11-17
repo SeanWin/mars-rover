2d6b3291d54d258d956204006891ef8dfa3c80e7
Sean Winston <seanwinston@gmail.com>
2024-11-17 19:01:47 +0000

    feat: add inputInstructions method to UI class

--- src/main/java/org/example/UI.java
+++ src/main/java/org/example/UI.java
@@ -1,15 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class UI {
     private State state;
     private MissionControl missionControl;
     private Scanner scanner;
+    private List<Position> finalPositions;
 
     public UI() {
         state = State.SETUP_PLATEAU;
         scanner = new Scanner(System.in);
+        finalPositions = new ArrayList<>();
     }
 
     public State setupPlateau(){
@@ -37,6 +41,29 @@ public class UI {
                 return State.INPUT_INSTRUCTIONS;
             } catch (IllegalArgumentException e) {
                 System.out.println("Please reenter a position within plateau bounds");
+            }
+        }
+    }
+
+    public State inputInstructions(){
+        while (true){
+            try{
+                System.out.println("Enter instructions");
+                String input = scanner.nextLine();
+                Instruction[] instructions = InputParser.parseInstructions(input);
+                Rover rover = missionControl.rovers.getLast();
+                Position finalPosition = missionControl.executeInstructions(rover, instructions);
+                System.out.println(finalPosition.getX()+" "+finalPosition.getY()+" "+finalPosition.getDirection());
+                finalPositions.add(finalPosition);
+                System.out.println("Add another rover? (y/n):");
+                String choice = scanner.nextLine();
+                if (choice.equalsIgnoreCase("y")) {
+                    return State.DEPLOY_ROVER;
+                } else {
+                    return State.FINISH;
+                }
+            } catch (IllegalStateException e) {
+                System.out.println("Rover position will be invalid, please reenter valid instructions");;
             }
         }
     }
